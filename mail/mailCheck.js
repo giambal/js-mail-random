e30b3288aa379d590f8acafe1a65c33d6be77aba
giambal <gimmy_1996@hotmail.it>
2019-02-13 15:40:39 +0100

    prova2

--- mail/mailCheck.js
+++ mail/mailCheck.js
@@ -1,11 +1,11 @@
-var mailAdd = Array["mail1@hotmail.it" , "mail2@gmail.com" , "mail3@libero.it" ,"mail4@hotmail.it" , "mail5@outlook.it"];
+var mailAdd =["mail1@hotmail.it" , "mail2@gmail.com" , "mail3@libero.it" ,"mail4@hotmail.it" , "mail5@outlook.it"];
 
-var mailAdd = prompt("digita il tuo indirizza e-mail:");
+var mailInserita = prompt("digita il tuo indirizza e-mail:");
 
 
 for (var i=0 ; i< mailAdd.length ; i++ ){
-  console.log(mailAddArray[i]);
-  if (mailAdd==i) {
+  console.log(mailAdd[i]);
+  if (mailInserita==i) {
     alert("Benvenuto!");
   }else {
     alert("la tua mail non è corretta!");
